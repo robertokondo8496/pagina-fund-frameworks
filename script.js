fca84f14d86f75e3293fc9bf82d31650bbdd981a
Roberto Kondo <robertokondo8496@gmail.com>
2020-10-04 18:14:49 -0300

    Adicionado scripts para interação com a página
    Adição dos scripts no arquivo 'script.js' contendo o código que faz a
    interação da página com o usuário (dados inseridos via formulário,
    evento de passar o ponteiro do mouse sobre a imagem). Complementação da
    estilização do 'styles.css'.

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 // arquivo com os scripts em javascript
+
+$(document).ready(function(){
+    $("button").click(function(){
+        var x = $("form").serializeArray();
+        var nome = JSON.stringify(x[0].value);
+        var sobrenome = JSON.stringify(x[1].value);
+        var opcao = JSON.stringify(x[2].value);
+        alert(nome + " " + sobrenome + " selecionou a opção " + opcao);
+    })
+})
+
+function funcaoimagem(){
+    alert('Você passou o cursor pela imagem! :(');
+}
